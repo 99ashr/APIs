e57a3d382b007b044839f947f167ab7bd8095601
99ashr <gaurashish99@gmail.com>
2021-01-30 09:45:18 +0530

    adding_parameter

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get('/')
 async def hello():
     return {'Hello': 'World'}
+
+
+@app.get("/ component/{para}")
+async def component(component_id):
+    return {"ID": "component_id"}
