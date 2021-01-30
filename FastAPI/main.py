5f97f737598525559c2a710aa709bf186f2ea448
99ashr <gaurashish99@gmail.com>
2021-01-30 14:37:20 +0530

    package

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,14 @@
+from pydantic import BaseModel
 from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+class Package(BaseModel):
+    name: str
+    number: int
+    Description: Optional[str] = None
 
 
 @app.get('/')
@@ -9,6 +16,19 @@ async def hello():
     return {'Hello': 'World'}
 
 
-@app.get("/ component/{para}")
-async def component(component_id):
-    return {"ID": "component_id"}
+@app.post('/package/{priority}')
+async def make_package(priority: int, package: Package, value: bool):
+    return {'priority': priority, **package.dict(), 'value': value}
+
+# -------------------------- Try it out(additional) -------------------------- #
+
+
+@app.get("/ component/{para}")  # @path parameter
+async def component(component_id: int):
+    return {"ID": component_id}
+
+
+@app.get("/component/")  # @query parameter
+async def read_component(number: int, text: Optional[str]):
+    return{"number": number, "text": text}
+# ---------------------------------------------------------------------------- #
