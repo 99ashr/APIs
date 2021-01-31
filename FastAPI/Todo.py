3ec6f9f0355b2291a7efd974fceeab26e75ca88c
99ashr <gaurashish99@gmail.com>
2021-01-31 23:24:05 +0530

    Created_response_model

--- FastAPI/Todo.py
+++ FastAPI/Todo.py
@@ -14,17 +14,17 @@ app = FastAPI(title="Todo API")
 store_todo = []
 
 
-@app.get('/')  # Home
+@ app.get('/')  # Home
 async def home():
     return {"Home": "Welcome Here!, 'Add a Todo on 127.0.0.1:8000/todo'"}
 
 
-@app.put('/todo/')  # Create
+@ app.put('/todo/')  # Create
 async def create_todo(todo: Todo):
     store_todo.append(todo)
     return todo
 
 
-@app.get('/todo/', response_model=List[Todo])  # Read
+@ app.get('/todo/', response_model=List[Todo])  # Read
 async def read_all_todo():
     return store_todo
--- FastAPI/main.py
+++ FastAPI/main.py
@@ -11,24 +11,37 @@ class Package(BaseModel):
     Description: Optional[str] = None
 
 
+class PackageIn(BaseModel):
+    secret_id: int
+    name: str
+    number: int
+    Description: Optional[str] = None
+
+
+# ------------------------------ Response model ------------------------------ #
+@app.post('/package/', response_model=Package)
+async def make_package(package: PackageIn):
+    return package
+
+
 @app.get('/')
 async def hello():
     return {'Hello': 'World'}
 
 
-@app.post('/package/{priority}')
-async def make_package(priority: int, package: Package, value: bool):
-    return {'priority': priority, **package.dict(), 'value': value}
+# @app.post('/package/{priority}')
+# async def make_package(priority: int, package: Package, value: bool):
+#     return {'priority': priority, **package.dict(), 'value': value}
 
 # -------------------------- Try it out(additional) -------------------------- #
 
 
-@app.get("/ component/{para}")  # @path parameter
-async def component(component_id: int):
-    return {"ID": component_id}
+# @app.get("/ component/{para}")  # @path parameter
+# async def component(component_id: int):
+#     return {"ID": component_id}
 
 
-@app.get("/component/")  # @query parameter
-async def read_component(number: int, text: Optional[str]):
-    return{"number": number, "text": text}
+# @app.get("/component/")  # @query parameter
+# async def read_component(number: int, text: Optional[str]):
+#     return{"number": number, "text": text}
 # ---------------------------------------------------------------------------- #
