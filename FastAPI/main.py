fbeab236fd9dc2db8b11b0f56c83a11899158fea
99ashr <gaurashish99@gmail.com>
2021-02-01 00:00:54 +0530

    modified

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -29,19 +29,19 @@ async def hello():
     return {'Hello': 'World'}
 
 
-# @app.post('/package/{priority}')
-# async def make_package(priority: int, package: Package, value: bool):
-#     return {'priority': priority, **package.dict(), 'value': value}
+@app.post('/package/{priority}')
+async def make_package(priority: int, package: Package, value: bool):
+    return {'priority': priority, **package.dict(), 'value': value}
 
 # -------------------------- Try it out(additional) -------------------------- #
 
 
-# @app.get("/ component/{para}")  # @path parameter
-# async def component(component_id: int):
-#     return {"ID": component_id}
+@app.get("/ component/{para}")  # @path parameter
+async def component(component_id: int):
+    return {"ID": component_id}
 
 
-# @app.get("/component/")  # @query parameter
-# async def read_component(number: int, text: Optional[str]):
-#     return{"number": number, "text": text}
+@app.get("/component/")  # @query parameter
+async def read_component(number: int, text: Optional[str]):
+    return{"number": number, "text": text}
 # ---------------------------------------------------------------------------- #
