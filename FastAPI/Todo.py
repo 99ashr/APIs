fde9c9421edf5a84523ac28f020d422afb7bb9d5
99ashr <gaurashish99@gmail.com>
2021-02-01 18:03:14 +0530

    Todo_list_crud_operations_created

--- FastAPI/Todo.py
+++ FastAPI/Todo.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 
 class Todo(BaseModel):
@@ -19,7 +19,7 @@ async def home():
     return {"Home": "Welcome Here!, 'Add a Todo on 127.0.0.1:8000/todo'"}
 
 
-@ app.put('/todo/')  # Create
+@ app.post('/todo/')  # Create
 async def create_todo(todo: Todo):
     store_todo.append(todo)
     return todo
@@ -28,3 +28,30 @@ async def create_todo(todo: Todo):
 @ app.get('/todo/', response_model=List[Todo])  # Read
 async def read_all_todo():
     return store_todo
+
+
+@app.get('/todo/{id}')
+async def get_todo(id: int):
+    try:
+        return store_todo[id-1]
+    except:
+        raise HTTPException(status_code=404, details="Todo Not Found")
+
+
+@app.put('/todo/{id}')
+async def update_todo(id: int, todo: Todo):
+    try:
+        store_todo[id] = todo
+        return store_todo[id]
+    except:
+        raise HTTPException(status_code=404, details="Todo Not Found")
+
+
+@app.delete('/todo/{id}')
+async def delete_todo(id: int):
+    try:
+        obj = store_todo[id]
+        store_todo.pop(id)
+        return obj
+    except:
+        raise HTTPException(status_code=404, details="Todo Not Found")
